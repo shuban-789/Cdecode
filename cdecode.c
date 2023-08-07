48051831100f8cb4d0001f2061c9885960100bda
Shuban <67974101+shuban-789@users.noreply.github.com>
2023-08-07 12:25:05 -0700

    Update cdecode.c

--- cdecode.c
+++ cdecode.c
@@ -4,43 +4,43 @@
 
 void docs(void);
 
-struct cipherkey
+struct cipherkey // Declare structure ciphersolvekey holding integer variables (since most keys are passed as integers)
 {
   int key;
 } ciphersolvekey;
 
-char* caesar(int key, char ciphertext[])
+char* caesar(int key, char ciphertext[]) // Caesar cipher algorithm
 {
-  int lenstr = strlen(ciphertext);
+  int lenstr = strlen(ciphertext); 
   char* res = malloc((lenstr + 1) * sizeof(char));
   for (int i = 0; i < lenstr; i++)
   {
-        if (ciphertext[i] >= 65 && ciphertext[i] <= 90)
+        if (ciphertext[i] >= 65 && ciphertext[i] <= 90) // If cipher in range of uppercase alphabet unicodes
         {
-            if (ciphertext[i] - key >= 65) {
-                res[i] = ciphertext[i] - key;
-            } else {
+            if (ciphertext[i] - key >= 65) { // If the key subtraction will still make it in the range of the uppercase alphabet unicodes
+                res[i] = ciphertext[i] - key; // Subtract the key from variable res (res will be the return variable)
+            } else { // Else, do a sort of rotation process via modulus to make sure it is in the range of uppercase alphabet unicodes
                 res[i] = ((ciphertext[i] - key - 65 + 26) % 26) + 65;
             }
         } 
-        else if (ciphertext[i] >= 97 && ciphertext[i] <= 122)
+        else if (ciphertext[i] >= 97 && ciphertext[i] <= 122) // Else if cipher in range of lowercase alphabet unicodes
         {
-            if (ciphertext[i] - key >= 97)
+            if (ciphertext[i] - key >= 97) // If the ciphertext's character's unicode with the key subtracted is still in range of uppercase alphabet unicode
             {
-                res[i] = ciphertext[i] - key;
+                res[i] = ciphertext[i] - key; // Just subtract the key from its unicode
             } 
-            else
+            else // Else
             {
-                res[i] = ((ciphertext[i] - key - 97 + 26) % 26) + 97;
+                res[i] = ((ciphertext[i] - key - 97 + 26) % 26) + 97; // Do the roation, but for the range of the uppercase alphabet only
             }
         }
         else
         {
-            res[i] = ciphertext[i];
+            res[i] = ciphertext[i]; // If it is not an alphabet character, just use the same character
         }
   }
-  res[lenstr] = '\0';
-  return res;
+  res[lenstr] = '\0'; // Handle the terminator
+  return res; // Return plaintext
 }
 
 void docs(void)
